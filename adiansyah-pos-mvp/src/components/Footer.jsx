b1d34883f7f05d5f43b30503422feec3ef370f40
dithub23 <dithub23@gmail.com>
2026-03-16 00:57:33 +0700

    Update footer link to correct GitHub repository

--- adiansyah-pos-mvp/src/components/Footer.jsx
+++ adiansyah-pos-mvp/src/components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="text-center p-6 text-sm text-gray-400">
       Task Eight by
       <a
-        href="https://github.com/dithub23/task-8-sinau-coding"
+        href="https://github.com/dithub23/dithub23-task-8-sinau-coding"
         className="text-orange-400 ml-2"
       >
         Adiansyah Mart
